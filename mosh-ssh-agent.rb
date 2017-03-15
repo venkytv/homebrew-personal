1c1273839b6a340cf2c498761ddc4b2819a34da4
Venky <venky@Venkys-Mac-mini.local>
2017-03-15 11:07:17 +0530

    Tweaks to the mosh formula

--- mosh-ssh-agent.rb
+++ mosh-ssh-agent.rb
@@ -1,4 +1,4 @@
-class MobileShellSshAgent < Formula
+class MoshSshAgent < Formula
   desc "Remote terminal application"
   homepage "https://mosh.org"
   url "https://api.github.com/repos/rinne/mosh/tarball/9ebe3ed2aad6a3c5bcf4c4c2a9d2ace097a53d03"
@@ -9,6 +9,8 @@ class MobileShellSshAgent < Formula
 
   deprecated_option "without-check" => "without-test"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "protobuf"
   depends_on :perl => "5.14" if MacOS.version <= :mountain_lion
@@ -19,7 +21,7 @@ class MobileShellSshAgent < Formula
     # PATH to support launching outside shell e.g. via launcher
     inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
 
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}", "--enable-completion", "--enable-agent-forwarding"
     system "make", "check" if build.with?("test") || build.bottle?
     system "make", "install"
